44d5347250328d343567ba59fd598118340719b7
fullmoonissue <styvodiabolo@gmail.com>
2018-07-26 17:28:53 +0200

    First track, first try

--- tas/scramble%/1-introduction.lua
+++ tas/scramble%/1-introduction.lua
@@ -1,8 +1,18 @@
 local input = require('tas/input')()
---local macro = require('tas/macro-collection')()
 
---local cf = 3128
-local cf = 3141
-cf = input:cross(cf, 300)
+-- Turbo start between 3128 / 3141 (included)
+local cf = 3128
+input:cross(cf, 805)
+cf = input:left(3350, 26)
+cf = input:left(cf + 25, 24)
+cf = input:left(cf + 25, 32)
+cf = input:left(cf + 34, 8)
+cf = input:left(cf + 18, 8)
+cf = input:right(cf + 25, 3)
+cf = input:right(cf + 210, 24)
+cf = input:right(cf + 30, 6)
+cf = input:right(cf + 30, 6)
+cf = input:right(cf + 30, 12)
+cf = input:square(cf + 15, 75)
 
 return input:all()
